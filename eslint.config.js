0827f7c5e19e57dc43ab16e5b9631979e112d506
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-04-21 01:59:42 +0900

    feat: prettier, eslint, typescript (w/ flat config)

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+    },
+  }
+);
